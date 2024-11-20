55bc2b0ea3569555c4dabeb3fd794b11538fba03
preeyaporn <preeyapornnr@gmail.com>
2024-11-20 13:53:43 +0700

    19

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,15 @@
+def count_to_25():
+  while True:
+    try:
+      num = int(input("Enter a number less than 25: "))
+      if num > 25:
+        print("Error")
+      else:
+        for i in range(num, 26):
+          print(f"Inside the loop, my variable is {i}")
+        break
+    except ValueError:
+      print("Invalid input. Please enter a number.")
+
+if __name__ == "__main__":
+  count_to_25()
