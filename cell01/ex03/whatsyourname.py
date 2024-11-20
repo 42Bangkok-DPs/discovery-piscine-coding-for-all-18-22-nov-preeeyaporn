7c4d538773cd9b8641660c79d5e246d56a326b69
preeeyaporn <preeyapornnr@gmail.com>
2024-11-20 11:53:40 +0700

    whatsyourname.py

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,5 +1,4 @@
-first_name = "Preeyaporn"
-last_name = "Narksomboon"
-print(f"Hey, what's your first name? : {first_name}")
-print(f"And your last name? : {last_name}")
-print(f"Well, pleased to meet you, {first_name} {last_name}")
+fname = input("Hey, what's your first name? : ")
+fname = fname.strip()
+lname  = input("And your last name? : ")
+print("Well, pleased to meet you,", fname +" "+ lname)
