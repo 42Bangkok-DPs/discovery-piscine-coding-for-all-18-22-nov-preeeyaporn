f811663992b55998afeed36d52deedb4bc62bbb0
preeyaporn <preeyapornnr@gmail.com>
2024-11-20 10:39:13 +0700

    14

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,5 @@
+first_name = "Preeyaporn"
+last_name = "Narksomboon"
+print(f"Hey, what's your first name? : {first_name}")
+print(f"And your last name? : {last_name}")
+print(f"Well, pleased to meet you, {first_name} {last_name}")
