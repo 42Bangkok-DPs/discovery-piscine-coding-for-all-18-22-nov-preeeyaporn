33386f884ceb4868980f0587cf15d7fc8212e296
preeeyaporn <preeyapornnr@gmail.com>
2024-11-21 02:27:29 +0000

    34

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 def main():
-  while True:
-    user_input = input("What you gotta say? ")
-    if user_input.lower() == "stop":
-      break
-    print("I got that! Anything else?")
+    while True:
+        user_input = input("What you gotta say? ")
+        if user_input == "STOP":
+            break
+        print("I got that! Anything else?")
 
 if __name__ == "__main__":
-  main()
+    main()
